57ac939c8ae0e07d76477a09edb48ac469ef8f0c
jklancic <jernej.klancic@appdirect.com>
2018-03-04 17:48:53 -0700

    Section 22 Lecture 281

--- src/main/java/guru/springfamework/controllers/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springfamework.domain.Category;
@@ -26,5 +27,15 @@ public class CategoryController {
 	@GetMapping
 	public ResponseEntity<List<Category>> getAllCategories() {
 		return new ResponseEntity<List<Category>>(categoryService.getAllCategories(), HttpStatus.OK);
+	}
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+		return new ResponseEntity<Category>(categoryService.getCategory(id), HttpStatus.OK);
+	}
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
+		return new ResponseEntity<Category>(categoryService.getCategory(name), HttpStatus.OK);
 	}
 }
